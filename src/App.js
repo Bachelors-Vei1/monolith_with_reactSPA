91a323efba244f6d7eef01408cab2592e2611135
Nikita Hvoina <nikita.hvoina@sapiens.com>
2024-04-20 17:57:32 +0300

    Īnit commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MicrofrontendInfo from './components/MicrofrontendInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <MicrofrontendInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    port: 8085,
+    contentBase: './dist',
+    hot: true
+  }
+};
