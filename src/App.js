2416b64fda958a51090d154a86160ac2a07b5057
Vei1x <nukleon13@gmail.com>
2024-04-23 03:49:59 +0300

    Added routes to new pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import MicrofrontendInfo from './components/MicrofrontendInfo';
+import React, {lazy, useState, useEffect, Suspense} from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"; // Import Switch instead of Routes
+import Navigation from './components/Navigation';
+
+const HomeLazy = lazy(() => import('./components/Home'));
+const SignUpLazy = lazy(() => import('./components/auth/SignUp'));
+const SignInLazy = lazy(() => import('./components/auth/SignIn'));
+const AboutLazy = lazy(() => import('./components/About'))
 
 function App() {
   return (
-    <div className="App">
-      <MicrofrontendInfo />
-    </div>
+    <BrowserRouter>
+      <Navigation/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch> {/* Use Switch instead of Routes */}
+          <Route exact path="/" component={HomeLazy}/> {/* Use component prop */}
+          <Route path="/auth/signin" component={SignInLazy}/>
+          <Route path="/auth/signup" component={SignUpLazy}/>
+          <Route path="/about" component={AboutLazy}/>
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
